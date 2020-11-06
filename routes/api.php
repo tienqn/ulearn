725ed6c818a96cae08efd7c546d9a3580dba161a
tienpham <tiens8acc@enjoyworks.co.kr>
2020-11-06 23:23:25 +0700

    cm

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('upload', function(Request $request){
+	return response()->json('hi!');
+});
