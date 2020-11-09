34e2ec2629501095e4cbe77e78aa81a7d2a74496
tienpham <tiens8acc@enjoyworks.co.kr>
2020-11-09 09:48:43 +0700

    local

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('upload', function(Request $request){
 
-	// $path = Storage::disk('s3')->put('images/originals', $request->file, 'public');
+	$path = Storage::disk('local')->put('images/originals', $request->file);
 
-	$exists = Storage::disk('s3')->exists('images/originals/D6KeWorzlylcAt51Y0tq43kqRPPYhTdgqjAaL9IF.txt');
+	// $exists = Storage::disk('s3')->exists('images/originals/D6KeWorzlylcAt51Y0tq43kqRPPYhTdgqjAaL9IF.txt');
 
-	return response()->json($exists);
+	return response()->json($path);
 });
